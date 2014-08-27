08e5ea1da1ad171db45c2d3e66fd88ab6f859f02
Bruno Vieira <mail@bmpvieira.com>
2014-08-27 02:12:38 +0100

    Add support for new limit and throughput options to CLI

--- cli.js
+++ cli.js
@@ -1,21 +1,40 @@
 #!/usr/bin/env node
+var minimist = require('minimist')
 var JSONStream = require('JSONStream')
 var ncbi = require('./')
 
-var args = process.argv.slice(2)
+var minimistOptions = {
+  alias: {
+    limit: 'l',
+    throughput: 't'
+  }
+}
 
-var command = args[0]
-var arg1 = args[1]
+var argv = minimist(process.argv.slice(2), minimistOptions)
+
+var command = argv._[0]
+var arg1 = argv._[1]
 
 if (command === 'link') {
-  var arg2 = args[2]
-  var arg3 = args[3]
+  var arg2 = argv._[2]
+  var arg3 = argv._[3]
 }
 else {
-  var arg2 = args.slice(2).join(' ')
+  var arg2 = argv._.slice(2).join(' ')
   var arg3 = null
 }
 
-var ncbiStream = ncbi[command](arg1, arg2, arg3)
+var options
+if (Object.keys(argv).length > 1) {
+  options = {
+    limit: argv.limit,
+    throughput: argv.throughput,
+    db: arg1,
+    term: arg2
+  }
+}
+
+
+var ncbiStream = options ? ncbi[command](options) : ncbi[command](arg1, arg2, arg3)
 
 ncbiStream.pipe(JSONStream.stringify(false)).pipe(process.stdout)
